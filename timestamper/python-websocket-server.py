946571471022b76edd2aaf4f8b8a720648c2444e
James Jelenko <jamesjelenko@gmail.com>
2024-04-23 01:15:24 -0700

    [ chrome-extensions ] — [ 2024-04-23 ]: add basic implementation to test out GUIO

--- timestamper/python-websocket-server.py
+++ timestamper/python-websocket-server.py
@@ -9,10 +9,9 @@ async def handle_client(websocket, path):
         headline = data["headline"]
         timestamp = data["timestamp"]
         event = data["event"]
-        
         # Print headline in Tkinter window
         # Display timestamp and event beneath the headline
-        
+
 async def main():
     async with websockets.serve(handle_client, "localhost", 8765):
         await asyncio.Future()  # Run forever
