49df02b14a42bde26f6a5b14c276aac6ab5d6994
James Jelenko <jamesjelenko@gmail.com>
2024-04-23 01:14:02 -0700

    [ chrome-extensions ] — [ 2024-04-23 ]: update background.js to add some debug logs

--- timestamper/background.js
+++ timestamper/background.js
@@ -1,13 +1,20 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    console.log('page loaded')
+    if (changeInfo.status === 'complete') {
+        console.log('executing script')
+        chrome.tabs.executeScript(tabId,{file: 'content.js'});
+    }
+
     // Connect to WebSocket server
+    console.log('opening socket')
     const socket = new WebSocket('ws://localhost:8765');
 
-    socket.onopen = function(event) {
-        console.log('Connected to WebSocket server');
-    };
+  socket.onopen = function(event) {
+      console.log('Connected to WebSocket server');
+  };
 
     // Handle messages from content script (if needed)
-    socket.onmessage = function(event) {
-        console.log('Received message from content script:', event.data);
-    };
+   socket.onmessage = function(event) {
+     console.log('Received message from content script:', event.data);
+   };
 });
