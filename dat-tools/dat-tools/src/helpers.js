65caf142a229fdd918ae8938315633ff95d99be5
James Jelenko <jamesjelenko@gmail.com>
2024-07-20 21:21:09 -0700

    add helper func for time, add lets, exports

--- dat-tools/dat-tools/src/helpers.js
+++ dat-tools/dat-tools/src/helpers.js
@@ -1,8 +1,13 @@
 const addLabels = (a, b) => {
-    spans = document.querySelectorAll('td[rowspan="1"]');
-    markA = a ? a : "A:";
-    markB = b ? b : "B:";
+    let spans = document.querySelectorAll('td[rowspan="1"]');
+    let markA = a ? a : "A:";
+    let markB = b ? b : "B:";
     spans[1].innerHTML = markA + '\n' + spans[1].innerHTML;
     spans[0].innerHTML = markB + '\n' + spans[0].innerHTML;
 }
 
+const fixTimes = () => {
+    Array.from($('time')).forEach((el)=>el.innerText+= '\n' + `${new Date(el.dateTime*1).toLocaleTimeString()}`)
+}
+
+module.exports = { fixTimes: fixTimes, addLabels: addLabels }
