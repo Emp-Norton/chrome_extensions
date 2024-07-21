3b91b2e3bc26421b16ec0826685d081f46ac9030
James Jelenko <jamesjelenko@gmail.com>
2024-07-20 21:20:41 -0700

    import helper funcs

--- dat-tools/dat-tools/src/App.js
+++ dat-tools/dat-tools/src/App.js
@@ -1,5 +1,6 @@
 import wiz from "./wizard.png";
 import './App.css';
+import {fixTimes, addLabels} from "./helpers";
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
                 <area id="left-eye" class="eye" target="" alt="" title="" href="" coords="456,346,21" shape="circle"/>
                 <area id="right-eye" class="eye" target="" alt="" title="" href="" coords="545,341,20" shape="circle"/>
                 <area id="label-spell" target="" alt="Labels" title="Labels" href=""
-                      coords="715,383,688,593,841,631,870,641,996,605,1009,385,966,344,857,391,752,350" shape="poly"/>
-                <area id="time-spell" target="" alt="Times" title="Times" href="" coords="154,516,127" shape="circle"/>
+                      coords="715,383,688,593,841,631,870,641,996,605,1009,385,966,344,857,391,752,350" shape="poly"
+                      onClick={()=> addLabels()}/>
+                <area id="time-spell" target="" alt="Times" title="Times" href="" coords="154,516,127" shape="circle"
+                      onClick={()=> fixTimes()}/>
             </map>
             <div className="right-eyeball eyes"></div>
             <div className="left-eyeball eyes"></div>
